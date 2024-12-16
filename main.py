dba2b39729da9ff63f69532a469cd045e2a046cc
Adeola Adejuwonlo <adejuwonlookanlawon@gmail.com>
2024-12-17 00:44:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from config.logging_config import setup_logging
 from services.telegram_service import TelegramService
 
@@ -6,10 +7,26 @@ logger = logging.getLogger(__name__)
 
 def main():
     setup_logging()
-    
+    start_time = time.time()
+    run_duration = 300  # Run the script for 60 seconds
+
     try:
         telegram_service = TelegramService()
         telegram_service.start()
+        
+        while True:
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+            
+            if elapsed_time > run_duration:
+                logger.info(f"Time's up! {run_duration} seconds have passed. Stopping the script.")
+                break
+            
+            # Add the logic you want to run in each loop iteration
+            logger.info("Script is still running...")
+            
+            # Sleep for a short while to avoid 100% CPU usage
+            time.sleep(5)  # Sleep for 5 seconds (You can adjust this)
         
     except Exception as e:
         logger.error(f"Failed to start bot: {e}")

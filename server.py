707cbaed66a356607a8323f679e3cafcc2b9f752
limFakson <fakeyejoshua2005@gmail.com>
2025-01-15 22:27:37 +0100

    update in commit

--- server.py
+++ server.py
@@ -5,11 +5,12 @@ import time
 
 app = Flask(__name__)
 
-@app.route('/run', methods=['GET'])
+
+@app.route("/run", methods=["GET"])
 def run_main_script():
     def run_script():
         # Run the main.py script
-        process = subprocess.Popen(['python3', 'main.py'])
+        process = subprocess.run(["python", "main.py"])
         # Wait 60 seconds (or however long you want the script to run)
         time.sleep(300)
         # Kill the process after 300 seconds
@@ -18,7 +19,8 @@ def run_main_script():
     thread = threading.Thread(target=run_script)
     thread.start()
 
-    return 'main.py script started and will stop after 300 seconds!'
+    return "main.py script started and will stop after 300 seconds!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
--- test/auto_test.py
+++ test/auto_test.py
@@ -11,4 +11,26 @@ def test_scraper():
         f.write(response.text)
 
 
-test_scraper()
+def get_data_from_api(url):
+    params = {
+        "operationName": "HomepageContentFeed",
+        "variables": {"filter": "Latest", "startIndex": 0, "newsOnly": False},
+        "extensions": {
+            "persistedQuery": {
+                "version": 1,
+                "sha256Hash": "6b442d2f8fc537c15b1fb67a2221ed3eca4e06c4b85e7818a220968cd917f0ae",
+            }
+        },
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "Python-Requests",
+    }
+
+    responce = requests.post(url, json=params, headers=headers)
+    with open("test/ign.json", "w") as f:
+        f.write(str(responce.json()))
+    return responce.json
+
+
+print(get_data_from_api("https://mollusk.apis.ign.com/graphql"))

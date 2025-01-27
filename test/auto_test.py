f86ce94f62d51510ac73134bb941a91e1f26a588
limFakson <fakeyejoshua2005@gmail.com>
2025-01-27 09:24:36 +0100

    error pushing secret

--- test/auto_test.py
+++ test/auto_test.py
@@ -37,19 +37,6 @@ def get_data_from_api(url):
 
 print(get_data_from_api("https://mollusk.apis.ign.com/graphql"))
 
-# from openai import OpenAI
-
-# client = OpenAI(
-#     api_key="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"
-# )
-
-# completion = client.chat.completions.create(
-#     model="gpt-3.5-turbo",
-#     messages=[
-#         {"role": "system", "content": "You are a helpful assistant."},
-#         {"role": "user", "content": "Write a haiku about recursion in programming."},
-#     ],
-# )
 
 # print(completion.choices[0].message)
 

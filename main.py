66fa5041b1196d66d25cbe441afd9d22a4d91abf
Joshua Opeyemi <fakeyejoshua2005@gmail.com>
2025-03-29 14:04:16 +0100

    new thread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import threading
 from config.logging_config import setup_logging
 from services.telegram_service import TelegramService
 
@@ -9,27 +10,27 @@ logger = logging.getLogger(__name__)
 def main():
     setup_logging()
     start_time = time.time()
-    run_duration = 10  # Run the script for 60 seconds
+    run_duration = 60  # Run the script for 60 seconds
 
     try:
         telegram_service = TelegramService()
         # telegram_service.start()
+        # print("Telegram service started")
+        # while True:
+        #     current_time = time.time()
+        #     elapsed_time = current_time - start_time
 
-        while True:
-            current_time = time.time()
-            elapsed_time = current_time - start_time
+        #     if elapsed_time > run_duration:
+        #         logger.info(
+        #             f"Time's up! {run_duration} seconds have passed. Stopping the script."
+        #         )
+        #         break
 
-            if elapsed_time > run_duration:
-                logger.info(
-                    f"Time's up! {run_duration} seconds have passed. Stopping the script."
-                )
-                break
+        #     # Add the logic you want to run in each loop iteration
+        #     logger.info("Script is still running...")
 
-            # Add the logic you want to run in each loop iteration
-            logger.info("Script is still running...")
-
-            # Sleep for a short while to avoid 100% CPU usage
-            time.sleep(5)  # Sleep for 5 seconds (You can adjust this)
+        #     # Sleep for a short while to avoid 100% CPU usage
+        #     time.sleep(5)  # Sleep for 5 seconds (You can adjust this)
 
     except Exception as e:
         logger.error(f"Failed to start bot: {e}")
@@ -37,4 +38,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    thread = threading(main(), daemon=True)
+    thread.start()
